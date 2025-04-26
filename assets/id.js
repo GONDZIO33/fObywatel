37f357f7c7733851512ea27528af5da7230976ad
GONDZIO33 <82333483+GONDZIO33@users.noreply.github.com>
2025-04-26 19:19:35 +0200

    Update id.js

--- assets/id.js
+++ assets/id.js
@@ -1,5 +1,3 @@
-
-
 var time = document.getElementById("time");
 var params = new URLSearchParams(window.location.search);
 
@@ -11,9 +9,9 @@ function delay(time) {
 
 var date = new Date();
 setClock();
-function setClock(){
-    date = new Date()
-    time.innerHTML = "Czas: " + date.toTimeString().split(" ")[0] + " " + date.toLocaleDateString("pl-PL", options);    
+function setClock() {
+    date = new Date();
+    time.innerHTML = "Czas: " + date.toTimeString().split(" ")[0] + " " + date.toLocaleDateString("pl-PL", options);
     delay(1000).then(() => {
         setClock();
     })
@@ -45,8 +43,8 @@ function getMobileOperatingSystem() {
   return 4;
 }
 
-if (getMobileOperatingSystem() == 2){
-    document.querySelector(".bottom_bar").style.height = "70px"
+if (getMobileOperatingSystem() == 2) {
+    document.querySelector(".bottom_bar").style.height = "70px";
 }
 
 let manifestElem = document.createElement('link');
@@ -56,56 +54,57 @@ document.head.prepend(manifestElem);
 
 var unfold = document.querySelector(".info_holder");
 unfold.addEventListener('click', () => {
-
-  if (unfold.classList.contains("unfolded")){
-    unfold.classList.remove("unfolded");
-  }else{
-    unfold.classList.add("unfolded");
-  }
-
+    if (unfold.classList.contains("unfolded")) {
+        unfold.classList.remove("unfolded");
+    } else {
+        unfold.classList.add("unfolded");
+    }
 })
 
-document.querySelector(".id_own_image").style.backgroundImage = `url(${params.get("image")})`;
+// 🛠️ Ładowanie danych z URL
 
-var birthday = params.get("birthday");
-var sex = params.get("sex");
+document.querySelector(".id_own_image").style.backgroundImage = `url('${params.get("image")}')`;
 
-setData("name", params.get("name").toUpperCase());
-setData("surname", params.get("surname").toUpperCase());
-setData("nationality", params.get("nationality").toUpperCase());
+var birthday = params.get("birthday") || "";
+var sex = params.get("sex") || "";
+
+setData("name", (params.get("name") || "").toUpperCase());
+setData("surname", (params.get("surname") || "").toUpperCase());
+setData("nationality", (params.get("nationality") || "").toUpperCase());
 setData("birthday", birthday);
-setData("familyName", params.get("familyName"));
-setData("sex", sex);
-setData("fathersFamilyName", params.get("fathersFamilyName"));
-setData("mothersFamilyName", params.get("mothersFamilyName"));
-setData("birthPlace", params.get("birthPlace"));
-setData("countryOfBirth", params.get("countryOfBirth"));
-setData("adress", "ul. " + params.get("adress1") + "<br>" + params.get("adress2") + " " + params.get("city"));
-setData("checkInDate", params.get("checkInDate"));
+setData("familyName", params.get("familyName") || "");
+setData("sex", sex || "");
+setData("fathersFamilyName", params.get("fathersFamilyName") || "");
+setData("mothersFamilyName", params.get("mothersFamilyName") || "");
+setData("birthPlace", params.get("birthPlace") || "");
+setData("countryOfBirth", params.get("countryOfBirth") || "");
+setData("adress", params.get("adress") || ""); // 🚀 Tutaj pobierasz cały adres
+setData("checkInDate", params.get("checkInDate") || "");
 
-var birthdaySplit = birthday.split(".");
-var day = birthdaySplit[0];
-var month = birthdaySplit[1];
-var year = birthdaySplit[2];
+// 🛠️ Automatyczne generowanie PESEL
+if (birthday !== "" && sex !== "") {
+    var birthdaySplit = birthday.split(".");
+    var day = birthdaySplit[0];
+    var month = birthdaySplit[1];
+    var year = birthdaySplit[2];
 
-if (parseInt(year) >= 2000){
-  month = 20 + parseInt(month);
+    if (parseInt(year) >= 2000) {
+        month = (20 + parseInt(month)).toString();
+    }
+
+    var later;
+    if (sex.toLowerCase() === "mężczyzna") {
+        later = "0295";
+    } else {
+        later = "0382";
+    }
+
+    var pesel = year.substring(2) + month + day + later + "7";
+    setData("pesel", pesel);
 }
 
-var later;
-
-if (sex.toLowerCase() === "mężczyzna"){
-  later = "0295"
-}else{
-  later = "0382"
+function setData(id, value) {
+    if (document.getElementById(id)) {
+        document.getElementById(id).innerHTML = value;
+    }
 }
-
-var pesel = year.substring(2) + month + day + later + "7";
-setData("pesel", pesel)
-
-function setData(id, value){
-
-  document.getElementById(id).innerHTML = value;
-
-}
-
